81254c66b01e13200754725337929fe0fe9ba67f
Luc626 <lucasst626@gmail.com>
2024-04-25 17:09:16 +0200

    esta puta mierda ya vale, el focking dibujo y el focking chat juntos

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+const {initDB, insertarMensaje, read} = require("./script/model")  
+
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.static('public'));
 
@@ -7,6 +14,25 @@ app.get('/', (req, res) => {
     res.send('¡Hola, mundo!');
   });
 
-app.listen(3000, () => {
+
+app.get( "/reset", ( req, res )=>{
+   initDB()
+  res.send( "Base de datos reiniciada" );
+})
+  io.on('connection', (socket) => {
+    console.log("toy joya")
+    const mensajes = read();
+      io.emit('init chat', mensajes);
+      socket.on('chat message', (msg)=>{
+        insertarMensaje(msg)
+      io.emit('chat message', msg)
+      })});
+    
+    // socket.on('paint', (datos) => {
+    //      io.emit('paint', datos);
+    //      console.log(datos);
+    //    });
+
+server.listen(3000, () => {
     console.log('Servidor iniciado en el puerto 3000');
   });
--- public/script.js
+++ public/script.js
@@ -5,12 +5,8 @@ const elemento = $('section')
       start: function() {
         isDragging = true;
       },
-      drag: function() {
-        console.log("arrastrando")
-
-      },
       stop: function() {
-        console.log("me has soltado")
+        isDragging = false
       }
     });
   } );
@@ -26,20 +22,52 @@ function toggleChat(){
   }
 }
  
+const socket = io();
+
+  const form = document.getElementById('formulario');
+  const input = document.getElementById('inputTexto');
+  const messages = document.getElementById('mensajes');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (input.value) {
+      socket.emit('chat message', input.value);
+      input.value = '';
+    }
+  });
+
+  socket.on('init chat', (mensajes)=>{
+mensajes.forEach(mensajeObjeto =>{
+const li = document.createElement("li");
+li.innerHTML = mensajeObjeto.mensaje;
+messages.appendChild(li)
+})  
+});
+
+  socket.on('chat message', (msg) => {
+    const item = document.createElement('li');
+    item.textContent = msg;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
     if(mouseIsPressed && !isDragging) {
+      if(mouseX !== pmouseX || mouseY !==  pmouseY){
         const datos = {
             x: mouseX,
-            y: mouseY
-        }
-        //socket.emit("paint", datos)
+            y: mouseY,
+            px: pmouseX,
+            py: pmouseY}
+      }
+      isDragging=true;
       fill(0);
-      ellipse(mouseX, mouseY, 20)
-    }
-}
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    }}
 
  
