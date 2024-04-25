b1a21a8df883e882333ea1aebc9b19c1e0e85979
idaira <idairaconejillo@gmail.com>
2024-04-25 16:57:46 +0200

    funciones basicas funciona bien y bonito subiendo todo yea

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+const { initDB, insertarMensaje, read } = require("./script/model.js");
+
+
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.static('public'));
 
@@ -7,6 +15,32 @@ app.get('/', (req, res) => {
     res.send('¡Hola, mundo!');
   });
 
-app.listen(3000, () => {
+  io.on('connection', (socket) => {
+    console.log('se ta metio un poligonero aki');
+    const mensajes = read()
+    io.emit("init chat", mensajes)
+    socket.on('chat message', (msg)=> {
+      insertarMensaje(msg)
+      io.emit('chat message', msg);
+    })
+    socket.on('disconnect', () => {
+      console.log('poligonero EJEKUTAO');
+    });
+    app.get("/reset", (req,res) => {
+    initDB();
+    res.send("base de datos resetiaaaada")
+    });
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+    socket.on('paint', (datos) => {
+      console.log(datos);
+      io.emit('paint', datos);
+
+    });
+
+  });
+
+server.listen(3000, () => {
     console.log('Servidor iniciado en el puerto 3000');
   });
--- public/script.js
+++ public/script.js
@@ -35,8 +35,35 @@ function draw() {
         }
         //socket.emit("paint", datos)
       fill(0);
-      ellipse(mouseX, mouseY, 20)
+      
+      line(mouseX,mouseY, pmouseX, pmouseY);
     }
 }
+const socket = io();
+const form = document.getElementById('formulario');
+const input = document.getElementById('inputtext');
+const messages = document.getElementById('mensajes');
 
- 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
+});
+
+socket.on('chat message', (msg) => {
+  const item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on('init chat', (mensajes)=> {
+  console.log(mensajes)
+  mensajes.forEach(mensajeObjeto =>{
+    const li = document.createElement("li")
+    li.innerHTML = mensajeObjeto.mensaje
+    messages.appendChild(li)
+  })
+});
