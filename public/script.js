0a8c8018b75148cba1a15962c38da8b406c1d015
idaira <idairaconejillo@gmail.com>
2024-04-30 14:52:34 +0200

    i need ur lovin

--- public/script.js
+++ public/script.js
@@ -42,39 +42,4 @@ function draw() {
     }
 }
 
-function convertirEnlaceAMarcaLista(mensaje) {
-  // Verificar si el mensaje contiene un enlace
-  if (mensaje.startsWith("https://")) {
-      // Crear un elemento de lista <li>
-      var listaItem = document.createElement("li");
-
-      // Crear un elemento de enlace <a>
-      var enlace = document.createElement("a");
-      enlace.href = mensaje; // Establecer el enlace href como el mensaje
-
-      // Añadir el texto del mensaje al enlace
-      enlace.appendChild(document.createTextNode(mensaje));
-
-      // Añadir el enlace al elemento de lista
-      listaItem.appendChild(enlace);
-
-      // Devolver el elemento de lista <li> con el enlace dentro
-      return listaItem;
-  } else {
-      // Si el mensaje no contiene un enlace, devolver el mensaje original
-      return mensaje;
-  }
-}
-
-
-var mensajeConEnlace = "https://www.ejemplo.com";
-var mensajeSinEnlace = "Este es un mensaje normal";
-
-var resultadoConEnlace = convertirEnlaceAMarcaLista(mensajeConEnlace);
-var resultadoSinEnlace = convertirEnlaceAMarcaLista(mensajeSinEnlace);
-
-console.log(resultadoConEnlace); // Imprime el elemento <li> con el enlace dentro
-console.log(resultadoSinEnlace); // Imprime el mensaje original, ya que no contiene un enlace
-
- 
  
